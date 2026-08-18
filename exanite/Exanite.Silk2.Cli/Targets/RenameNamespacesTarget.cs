1cd88140e97728ed637f906cb4f9564583a25b03
William Chen <exanite78012@gmail.com>
2026-08-17 20:12:31 -0400

    Implement RenameNamespacesTarget

--- exanite/Exanite.Silk2.Cli/Targets/RenameNamespacesTarget.cs
+++ exanite/Exanite.Silk2.Cli/Targets/RenameNamespacesTarget.cs
@@ -1,11 +1,50 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Exanite.Core.Io;
+using Exanite.Core.Utilities;
+using Exanite.Engine.BuildSystem.Utilities;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Rename;
 
 namespace Exanite.Silk2.Cli.Targets;
 
 public class RenameNamespacesTarget
 {
+    private const string SolutionName = "Silk.NET.slnx";
+
     public async Task Run()
     {
+        MSBuildLocator.RegisterDefaults();
 
+        GuardUtility.IsTrue(AbsolutePath.WorkingDirectory.TryFindFileUpwards(SolutionName, out var solutionPath), $"Failed to find solution: {SolutionName}");
+
+        $"Found solution: {solutionPath}".Dump();
+
+        using var workspace = MSBuildWorkspace.Create();
+        var solution = await workspace.OpenSolutionAsync(solutionPath);
+        var originalSolution = solution;
+
+        $"Opened solution with {solution.ProjectIds.Count} projects".Dump();
+
+        var coreProject = solution.Projects.First(x => x.Name == "Silk.NET.Core");
+        var compilation = GuardUtility.NotNull(await coreProject.GetCompilationAsync());
+        var silkNamespaceSymbol = compilation.GlobalNamespace.GetNamespaceMembers().First(x => x.Name == "Silk");
+
+        $"Renaming Silk namespace to Silk2, this might take a while".Dump();
+
+        solution = await Renamer.RenameSymbolAsync(solution, silkNamespaceSymbol, new SymbolRenameOptions()
+        {
+            RenameFile = false,
+            RenameInComments = false,
+            RenameInStrings = false,
+            RenameOverloads = true,
+        }, "Silk2");
+
+        "Applying changes to disk".Dump();
+
+        await RoslynUtility.ApplyChanges(originalSolution, solution);
+
+        "Rename completed".Dump();
     }
 }
